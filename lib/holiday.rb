94cefea339b129cea6f62804e9f25dd85c58a328
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-20 10:05:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -100,7 +100,6 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_value.each do |holiday_key, supplies|
       if supplies.include?("BBQ")
         bbq_holidays << holiday_key
-        #binding.pry
       end
     end
   end
