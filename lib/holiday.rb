7331970da491349bbc4e35f786311d303f24d3a0
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-20 09:46:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,8 +88,8 @@ def all_supplies_in_holidays(holiday_hash)
         #binding.pry
         holiday.collect {|word| word.capitalize!}
 #binding.pry
-        #holiday.join(' ')
-      #binding.pry
+        holiday.join(' ')
+      binding.pry
       puts "  #{holiday}: #{supplies_value.join(", ")}"
       #binding.pry
     end
